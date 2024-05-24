ef5b2663fbb62f5b34188e536f2e9a4390bf942a
Lucas E. <loangluci@gmail.com>
2024-05-24 20:59:27 +0200

    added advanced sensor functionality

--- Raspi_side/mqtt.py
+++ Raspi_side/mqtt.py
@@ -1,7 +1,47 @@
 import random
 from paho.mqtt import client as mqtt_client
 
+import datetime
+import MySQLdb
+import requests
 
+# Mysql Setup
+mydb = MySQLdb.connect(
+        host="localhost",
+        user="data_feeder",
+        password="smoke420",
+        database="advanced_weed_sensors"
+)
+
+mycursor = mydb.cursor()
+
+def decode_msg(msg):
+    data = []
+
+    for line in msg.splitlines():
+        if ":" in line:
+            line = line.split(":")[1]
+            data.append(line)
+
+    moisture = data[0]
+    air_temperature = data[1]
+    gnd_temperature = data[2]
+    pressure = data[3]
+    humidity = data[4]
+    co2 = data[5]
+    voc = data[6]
+    current_time = datetime.datetime.now().strftime("%Y_%m_%d_-_%H_%M_%S.%f")
+
+    print(moisture, air_temperature, gnd_temperature, pressure, humidity, co2, voc, current_time)
+
+def write_to_database(data):
+    SQL_COMMAND = "INSERT INTO sensor_data (moisture, air_temperature, gnd_temperature, pressure, humidity, co2, voc, time_stamp) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);"
+
+    mycursor.execute(SQL_COMMAND,data)
+    
+    mydb.commit()
+
+# MQTT Setup
 broker = '192.168.177.35'
 port = 1883
 topic = "weed_sensors"
@@ -27,8 +67,8 @@ def connect_mqtt() -> mqtt_client:
 
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
-        
-        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        data = decode_msg(msg.payload.decode())
+        #write_to_database(data)
 
     client.subscribe(topic)
     client.on_message = on_message
